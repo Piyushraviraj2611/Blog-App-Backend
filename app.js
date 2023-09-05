d664c901ae9b3a2117b9cc249dfc244c0163c543
Piyushraviraj2611 <piyushraviraj2611@gmail.com>
2023-09-06 00:42:15 +0530

    app js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const errorMiddleware = require("./middlewares/error");
 const cors = require("cors");
 const app = express();
 
-//app.use(cors({ origin: "https://full-stack-blog-app-frontend.netlify.app" }));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://blog-writefreely-adminpanel.onrender.com" }));
+//app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 const postRoutes = require("./routes/postRoutes");
 
 app.get("/", (req, res) => {
-	res.send('<h1>Working Fine. <a href="http://localhost:3000/">Click here</a> to go to frontend.');
-	//res.send('<h1>Working Fine. <a href="https://full-stack-blog-app-frontend.netlify.app/">Click here</a> to go to frontend.');
+	//res.send('<h1>Working Fine. <a href="http://localhost:3000/">Click here</a> to go to frontend.');
+	res.send('<h1>Working Fine. <a href="https://blog-writefreely-adminpanel.onrender.com/">Click here</a> to go to frontend.');
 });
 
 app.use("/api/v1/post", postRoutes);
